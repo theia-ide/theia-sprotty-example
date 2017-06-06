dabb8a768b34e3fee3017cf9a420b9fe48e31c78
Jan Koehnlein <jan.koehnlein@typefox.io>
2017-06-06 21:17:37 +0200

    [electron] first shot at electron app

---
+++ theia-electron/webpack.config.js
@@ -0,0 +1,7 @@
+const webpack = require("webpack");
+
+module.exports = require("./config/webpack/webpack.config")(__dirname, {
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+});
